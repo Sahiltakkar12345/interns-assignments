04a475b052f84a70ad05e6aae3c5609b3b7bb601
Sahiltakkar12345 <128775220+Sahiltakkar12345@users.noreply.github.com>
2024-01-20 15:30:31 +0530

    Update Arithmetic_evaluation.java

--- Arithmetic_evaluation.java
+++ Arithmetic_evaluation.java
@@ -8,8 +8,8 @@ import java.util.Stack;
 public class Arithmetic_evaluation {
     public static void main(String[] args) {
         try {
-            BufferedReader reader = new BufferedReader(new FileReader("abc.txt"));
-            BufferedWriter writer = new BufferedWriter(new FileWriter("newabc.txt"));
+            BufferedReader reader = new BufferedReader(new FileReader("input.txt"));
+            BufferedWriter writer = new BufferedWriter(new FileWriter("output.txt"));
 
             String line;
             while ((line = reader.readLine()) != null) {
